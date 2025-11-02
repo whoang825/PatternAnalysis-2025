7a63fc247376f148875e445a90163c4af85e93cc
whoang825 <w.hoang@student.uq.edu.au>
2025-11-02 14:17:22 +1000

    Simplified and updated the UNet architecture to extract less features per convolution block and removed the 5th layer during upsampling and downsampling

--- recognition/improved_unet_47996283/modules.py
+++ recognition/improved_unet_47996283/modules.py
@@ -9,7 +9,7 @@ class UNet(nn.Module):
     CNN for MRI Segmentation, outputting a segmentation mask representing the probability
     that the pixel belongs to the target region (e.g. brain tissue, tumor).
     """
-    def __init__(self, in_channels=1, out_channels=4, base_filters=64, dropout_p=0.2):
+    def __init__(self, in_channels=1, out_channels=4, base_filters=32, dropout_p=0):
         super().__init__()
 
         # Encoder (Downsampling)
@@ -18,16 +18,12 @@ class UNet(nn.Module):
         self.enc2 = self._conv_block(base_filters, base_filters * 2, dropout_p)  # 128 x 128
         self.enc3 = self._conv_block(base_filters * 2, base_filters * 4, dropout_p)  # 64 x 64
         self.enc4 = self._conv_block(base_filters * 4, base_filters * 8, dropout_p)  # 32 x 32
-        self.enc5 = self._conv_block(base_filters * 8, base_filters * 16, dropout_p)
 
         # Bottleneck
         # Deepest layers capturing abstract features
-        self.bottleneck = self._conv_block(base_filters * 16, base_filters * 32, dropout_p)
+        self.bottleneck = self._conv_block(base_filters * 8, base_filters * 16, dropout_p)
 
         # Decoder (Upsampling)
-        self.up5 = self._up_block(base_filters * 32, base_filters * 16)
-        self.dec5 = self._conv_block(base_filters * 32, base_filters * 16, dropout_p)
-
         self.up4 = self._up_block(base_filters * 16, base_filters * 8)
         self.dec4 = self._conv_block(base_filters * 16, base_filters * 8, dropout_p)
 
@@ -46,7 +42,21 @@ class UNet(nn.Module):
         # Pooling for encoder
         self.pool = nn.MaxPool2d(2, 2)
 
-    def _conv_block(self, in_ch, out_ch, dropout_p=0.3):
+        # Weight initialization
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # Better initialization for LeakyReLU
+                nn.init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='leaky_relu', a=0.1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _conv_block(self, in_ch, out_ch, dropout_p=0):
         """Conv block: Conv -> BN -> LeakyReLU -> Dropout -> Conv -> BN -> LeakyReLU -> Dropout
         To extract features from the input image.
         First layer extracts basic features like edges and corners.
@@ -55,11 +65,11 @@ class UNet(nn.Module):
         return nn.Sequential(
             nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(inplace=True),
+            nn.LeakyReLU(0.1, inplace=True),
             nn.Dropout2d(dropout_p),
             nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(inplace=True),
+            nn.LeakyReLU(0.1, inplace=True),
             nn.Dropout2d(dropout_p)
         )
 
@@ -79,26 +89,26 @@ class UNet(nn.Module):
         e2 = self.enc2(self.pool(e1))
         e3 = self.enc3(self.pool(e2))
         e4 = self.enc4(self.pool(e3))
-        e5 = self.enc5(self.pool(e4))
 
         # Bottleneck (global features of the image)
-        b = self.bottleneck(self.pool(e5))
+        b = self.bottleneck(self.pool(e4))
 
         # Decoder with skip connections
-        d5 = self.up5(b)
-        d5 = self.dec5(torch.cat([d5, e5], dim=1))
-
-        d4 = self.up4(d5)
-        d4 = self.dec4(torch.cat([d4, e4], dim=1))
+        d4 = self.up4(b)
+        d4 = torch.cat([d4, e4], dim=1)
+        d4 = self.dec4(d4)
 
         d3 = self.up3(d4)
-        d3 = self.dec3(torch.cat([d3, e3], dim=1))
+        d3 = torch.cat([d3, e3], dim=1)
+        d3 = self.dec3(d3)
 
         d2 = self.up2(d3)
-        d2 = self.dec2(torch.cat([d2, e2], dim=1))
+        d2 = torch.cat([d2, e2], dim=1)
+        d2 = self.dec2(d2)
 
         d1 = self.up1(d2)
-        d1 = self.dec1(torch.cat([d1, e1], dim=1))
+        d1 = torch.cat([d1, e1], dim=1)
+        d1 = self.dec1(d1)
 
         # Final output (B, 4, H, W)
         out = self.final_conv(d1)
@@ -155,10 +165,13 @@ class CombinedLoss(nn.Module):
     Combined Dice + CrossEntropy loss for better optimization
     """
 
-    def __init__(self, num_classes, dice_weight=0.7, ce_weight=0.3, smooth=1e-6):
+    def __init__(self, num_classes, class_weights, dice_weight=0.5, ce_weight=0.5, smooth=1e-6):
         super().__init__()
         self.dice_loss = DiceLoss(num_classes, smooth)
-        self.ce_loss = nn.CrossEntropyLoss()
+        if class_weights is not None:
+            self.ce_loss = nn.CrossEntropyLoss(weight=class_weights)
+        else:
+            self.ce_loss = nn.CrossEntropyLoss()
         self.dice_weight = dice_weight
         self.ce_weight = ce_weight
 
